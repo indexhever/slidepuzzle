b427b91b5c3477284b64e04e8e1e51734211e767
Heverton Sarah <heverton.sarah@gmail.com>
2020-04-01 22:41:43 -0300

    Detect touch on slot

---
+++ Assets/Scripts/SlotComponent.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+namespace Game
+{
+    public class SlotComponent : MonoBehaviour, IPointerClickHandler
+    {
+        private Slot slot;
+        private SlotSelection slotSelection;
+
+        public void Construct(SlotSelection slotSelection)
+        {
+            this.slotSelection = slotSelection;
+            slot = new SlotImplementation(slotSelection);
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            Debug.Log("Tocou no slot");
+            //slot.Touch();
+        }
+    }
+}
