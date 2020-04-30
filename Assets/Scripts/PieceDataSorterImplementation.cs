7b450a363d5d14c43754bd7188e9944744e2c4f0
Heverton Sarah <heverton.sarah@gmail.com>
2020-04-29 22:24:39 -0300

    Retrieve PieceDatas in random way

---
+++ Assets/Scripts/PieceDataSorterImplementation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Game
+{
+    public class PieceDataSorterImplementation : PieceDataSorter
+    {
+        private List<PieceData> pieceDataList;
+        private int currentRandomIndex;
+
+        public PieceDataSorterImplementation(List<PieceData> pieceDataList)
+        {
+            this.pieceDataList = pieceDataList;
+        }
+
+        public PieceData GetRandomPieceData()
+        {
+            currentRandomIndex = Random.Range(0, pieceDataList.Count);
+
+            PieceData newPieceData = pieceDataList[currentRandomIndex];
+            pieceDataList.RemoveAt(currentRandomIndex);
+
+            return newPieceData;
+        }
+    }
+}
---
+++ Assets/Tests/PieceSortingTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Game;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class PieceSortingTest
+    {
+        [Test]
+        public void PieceIsCorrectlyRetrieved()
+        {
+            List<PieceData> pieceDataList = CreatePieceDataList(3);
+            PieceDataSorter pieceSorter = CreatePieceDataSorter(pieceDataList);
+
+            PieceData pieceData = pieceSorter.GetRandomPieceData();
+
+            Assert.IsNotNull(pieceData);
+        }
+
+        [Test]
+        public void EachPieceDataTakenIsDifferent()
+        {
+            List<PieceData> pieceDataList = CreatePieceDataList(3);
+            PieceDataSorter pieceSorter = CreatePieceDataSorter(pieceDataList);
+
+            PieceData firstPieceData = pieceSorter.GetRandomPieceData();
+            PieceData secondPieceData = pieceSorter.GetRandomPieceData();
+
+            Assert.AreNotEqual(firstPieceData, secondPieceData);
+        }
+
+        [Test]
+        public void EachTimeDataIsRetrievedItIsRemovedFromList()
+        {
+            List<PieceData> pieceDataList = CreatePieceDataList(3);
+            PieceDataSorter pieceSorter = CreatePieceDataSorter(pieceDataList);
+
+            PieceData retrievedPieceData = pieceSorter.GetRandomPieceData();
+
+            Assert.IsFalse(pieceDataList.Contains(retrievedPieceData));
+        }
+
+        private List<PieceData> CreatePieceDataList(int amountOfData)
+        {
+            List<PieceData> pieceDataList = new List<PieceData>();
+
+            for(int i = 0; i < amountOfData; i++)
+            {
+                pieceDataList.Add(new StubPieceData());
+            }
+
+            return pieceDataList;
+        }
+
+        private PieceDataSorter CreatePieceDataSorter(List<PieceData> pieceDataList)
+        {
+            return new PieceDataSorterImplementation(pieceDataList);
+        }
+    }
+}
